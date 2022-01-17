abce5f2991c50de09f7fe7a4fb8d5a5dd388fdff
Weston Lu <westonlu@utexas.edu>
2022-01-16 20:07:35 -0600

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Weston Lu on 1/16/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
